7f99b49229e656f05dc6be68aff676295fcb2e00
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-21 18:55:51 -0400

    Updates

---
+++ precache-manifest.05ecde641b499ad9ee5eea38c1550df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eac6269ec5e56ab59e6d4566529ffac",
+    "url": "/personalwebsite/index.html"
+  },
+  {
+    "revision": "d280951f40a7e61a85d7",
+    "url": "/personalwebsite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/personalwebsite/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d280951f40a7e61a85d7",
+    "url": "/personalwebsite/static/js/main.44301908.chunk.js"
+  },
+  {
+    "revision": "1f782c562390b2e61027",
+    "url": "/personalwebsite/static/js/runtime~main.29a1488c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personalwebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c894455f40058a4b8cd086a54ba6002f.js"
+  "/personalwebsite/precache-manifest.05ecde641b499ad9ee5eea38c1550df4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personalwebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
