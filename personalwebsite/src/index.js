f8c54365c2bda698e4f6dba4962a8587c17b229c
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-23 15:43:37 -0400

    refactor: added new file

--- personalwebsite/src/index.js
+++ personalwebsite/src/index.js
@@ -5,13 +5,13 @@ import App from './components/App';
 ReactDOM.render(
 	<App
 		strings={[
-			"Hi, I'm Henry",
-			'I am a <strong><i>Software Developer</i></strong>',
-			'I graduated from <strong>Purdue University</strong>',
-			'I do <strong>Web Development</strong>',
-			'I do other exciting stuffs too',
-			'I am trilingual',
-			'I am a <strong>team player</strong>',
+			"Hi, I'm Henry.",
+			'I am a <strong><i>Software Developer.</i></strong>',
+			'I graduated from <strong>Purdue University.</strong>',
+			'I do <strong>Web Development.</strong>',
+			'I do other exciting stuffs too!',
+			'I am trilingual.',
+			'I am a <strong>team player.</strong>',
 		]}
 	/>,
 	document.querySelector('#root'),
