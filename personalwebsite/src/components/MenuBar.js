424263a42bbea7aaa4208dda410dcaae5cbe23d5
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-24 18:25:48 -0400

    refactor: changed directory path

--- personalwebsite/src/components/MenuBar.js
+++ personalwebsite/src/components/MenuBar.js
@@ -5,7 +5,7 @@ const MenuBar = () => {
 		<div class="ui container">
 			<div class="ui large secondary inverted pointing menu">
 				<div class="right item">
-					<a class="ui inverted button" href="/images/resume.pdf" download="Resume.pdf">
+					<a class="ui inverted button" href="../images/resume.pdf" download="Resume.pdf">
 						Resume
 					</a>
 					<a class="ui inverted button">Contact</a>
