082058f1ed01e5f2f0462d18c4782b0d5b79d00c
Ley Yen Choo <lchoo1996@gmail.com>
2019-06-24 18:24:37 -0400

    refactor: added resume pdf

--- personalwebsite/src/components/MenuBar.js
+++ personalwebsite/src/components/MenuBar.js
@@ -5,7 +5,9 @@ const MenuBar = () => {
 		<div class="ui container">
 			<div class="ui large secondary inverted pointing menu">
 				<div class="right item">
-					<a class="ui inverted button">Resume</a>
+					<a class="ui inverted button" href="/images/resume.pdf" download="Resume.pdf">
+						Resume
+					</a>
 					<a class="ui inverted button">Contact</a>
 				</div>
 			</div>
