b6ddc106a26c84326f1cd63abc58ba0943b3bc5d
Henry Ley Yen Choo <lchoo1996@gmail.com>
2019-06-24 22:23:11 -0400

    refactor: added new item

--- personalwebsite/src/components/MenuBar.js
+++ personalwebsite/src/components/MenuBar.js
@@ -4,9 +4,9 @@ const MenuBar = () => {
 	return (
 		<div class="ui container">
 			<div class="ui large secondary inverted pointing menu">
-				<div class="left item">
-					<a class>Projects</a>
-				</div>
+				
+					<a class="ui ">My Work</a>
+				
 				<div class="right item">
 					<a class="ui inverted button" href="./resume.pdf" download="resume.pdf">
 						Resume
