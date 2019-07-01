c730620edfa394baec8767b24f45df1f4e55766f
lchoo <lchoo1996@gmail.com>
2019-07-01 19:20:05 -0400

    refactor: added content to card

--- personalwebsite/src/components/Cards/Cards.js
+++ personalwebsite/src/components/Cards/Cards.js
@@ -1,51 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import "./Cards.css";
 const Cards = props => {
-	return (
-		// <div className="ui link cards">
-		// 	<div className="card">
-		// 		<div className="image">
-		// 			<img src="https://www.pexels.com/photo/nature-red-love-romantic-67636/" />
-		// 		</div>
-		// 		<div className="content">
-		// 			<div className="header">TEST</div>
-		// 			<div className="meta">
-		// 				<a>Friends</a>
-		// 			</div>
-		// 			<div className="description">BLABLABLABLABLA</div>
-		// 		</div>
-		// 		<div className="extra content">
-		// 			<span className="right floated">Joined in 2013</span>
-		// 			<span>
-		// 				<i className="user icon" />75 Friends
-		// 			</span>
-		// 		</div>
-		// 	</div>
-		// </div>
-		<div className="ui fluid link card">
-			<div className="image">
-				<img src={props.imgUrl} />
-			</div>
-			<div className="content">
-				<a className="header">{props.headerText}</a>
-				<div className="content">
-					<div className="header">TEST</div>
-
-					<div className="meta">
-						<a>Friends</a>
-					</div>
-					<div className="description">BLABLABLABLABLA</div>
-				</div>
-				<div className="extra content">
-					<span className="right floated">Joined in 2013</span>
-					<span>
-						<i className="user icon" />
-						75 Friends
-					</span>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="ui fluid link card">
+            <div className="image">
+                <img src={props.imgUrl} alt={props.imgAlt} />
+            </div>
+            <div className="content">
+                <a className="header">{props.headerText}</a>
+                <div className="meta">
+                    <a>{props.projectType}</a>
+                </div>
+                <div className="description">{props.projectDescription}</div>
+                <div className="extra content">
+                    <span className="right floated">Joined in 2013</span>
+                    <span>
+                        <i className="user icon" />
+                        75 Friends
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Cards;
--- personalwebsite/src/components/Content/Content.js
+++ personalwebsite/src/components/Content/Content.js
@@ -1,36 +1,46 @@
-import React from 'react';
-import './Content.css';
-import Card from '../Cards/Cards';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import React from "react";
+import "./Content.css";
+import Card from "../Cards/Cards";
+import fliegen from "../../images/fliegen.PNG";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 export default function Content({ id }) {
-	return (
-		<div className="ui vertical stripe segment" id={id}>
-			<button className="ui button" onClick={() => scroll.scrollToTop()}>
-				GO UP
-			</button>
-			<div className="ui middle aligned stackable grid container">
-				<div className="ui three column grid">
-					<div className="column">
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-					</div>
-					<div className="column">
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-					</div>
-					<div className="column">
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-						<Card imgUrl="https://html.com/wp-content/uploads/flamingo.jpg" headerText="TESTING" />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="ui vertical stripe segment" id={id}>
+            <button className="ui button" onClick={() => scroll.scrollToTop()}>
+                GO UP
+            </button>
+            <div className="ui middle aligned stackable grid container">
+                <div className="ui three column grid">
+                    <div className="column">
+                        <Card
+                            imgUrl={fliegen}
+                            imgAlt="fliegen"
+                            headerText="Fliegen"
+                            projectType="Web application"
+                            projectDescription="A web application designed for students and recruiters/professionals to connect more effectively"
+                        />
+                    </div>
+                    <div className="column">
+                        <Card
+                            imgUrl={fliegen}
+                            imgAlt="fliegen"
+                            headerText="Fliegen"
+                            projectType="Web application"
+                            projectDescription="BLABLABLA"
+                        />
+                    </div>
+                    <div className="column">
+                        <Card
+                            imgUrl={fliegen}
+                            imgAlt="fliegen"
+                            headerText="Fliegen"
+                            projectType="Web application"
+                            projectDescription="BLABLABLA"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
