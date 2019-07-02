6935823f2963841edaad08302112d2ac3b7bd2ea
Ley Yen Choo <lchoo1996@gmail.com>
2019-07-02 07:32:24 -0400

    refactor: changed content type and fix div

--- personalwebsite/src/components/Cards/Cards.js
+++ personalwebsite/src/components/Cards/Cards.js
@@ -1,27 +1,27 @@
-import React from "react";
-import "./Cards.css";
+import React from 'react';
+import './Cards.css';
 const Cards = props => {
-    return (
-        <div className="ui fluid link card">
-            <div className="image">
-                <img src={props.imgUrl} alt={props.imgAlt} />
-            </div>
-            <div className="content">
-                <a className="header">{props.headerText}</a>
-                <div className="meta">
-                    <a>{props.projectType}</a>
-                </div>
-                <div className="description">{props.projectDescription}</div>
-                <div className="extra content">
-                    <span className="right floated">Joined in 2013</span>
-                    <span>
-                        <i className="user icon" />
-                        75 Friends
-                    </span>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="ui fluid link card">
+			<div className="image">
+				<img src={props.imgUrl} alt={props.imgAlt} />
+			</div>
+			<div className="content">
+				<a className="header">{props.headerText}</a>
+				<div className="meta">
+					<h5>{props.projectType}</h5>
+				</div>
+				<div className="description">{props.projectDescription}</div>
+			</div>
+			<div className="extra content">
+				<span className="right floated">Joined in 2013</span>
+				<span>
+					<i className="user icon" />
+					75 Friends
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default Cards;
