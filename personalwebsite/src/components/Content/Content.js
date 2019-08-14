45fa78b2b0db2acae840b5b9441f3ff2bfcdb637
lchoo <lchoo1996@gmail.com>
2019-08-14 14:04:30 -0700

    refactor: added project title

--- personalwebsite/src/components/Content/Content.js
+++ personalwebsite/src/components/Content/Content.js
@@ -10,6 +10,22 @@ export default function Content({ id }) {
             <button className="ui button" onClick={() => scroll.scrollToTop()}>
                 GO UP
             </button>
+            <div
+                style={{
+                    textAlign: "center",
+                    margin: "0 auto 0 auto",
+                    paddingDown: "1rem",
+                }}
+            >
+                <h1
+                    style={{
+                        fontSize: "3rem",
+                        fontFamily: "Poppins, sans-serif",
+                    }}
+                >
+                    My Projects
+                </h1>
+            </div>
             <div className="ui middle aligned stackable grid container">
                 <div className="ui three column grid">
                     <div className="column">
