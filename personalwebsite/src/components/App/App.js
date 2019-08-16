7bad2422a4bc64d4a86e0c59d13e9f0cd0a464f8
lchoo <lchoo1996@gmail.com>
2019-08-16 14:33:25 -0700

    build: integrated scss

--- personalwebsite/src/components/App/App.js
+++ personalwebsite/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import './App.css';
-import './App.css'
+import "../../css/style.min.css";
 import Typed from 'typed.js';
 import MenuBar from '../Menu/MenuBar';
 import Content from '../Content/Content';
