8c6211088ff52c6973bb7bee0ce99729a626653c
lchoo <lchoo1996@gmail.com>
2019-08-21 13:58:42 -0700

    refactor: added provider to root

--- personalwebsite/src/index.js
+++ personalwebsite/src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+
+import store from "./store";
+import { Provider, ReactReduxContext } from "react-redux";
 
 ReactDOM.render(
-	<App
-		strings={[
-			"Hi, I'm Henry.",
-			'I am a <strong><i>Software Developer.</i></strong>',
-			'I graduated from <strong>Purdue University.</strong>',
-			'I do <strong>Web Development.</strong>',
-			'I do other exciting stuffs too!',
-			'I am trilingual.',
-			'I am a <strong>team player.</strong>',
-		]}
-	/>,
-	document.querySelector('#root'),
+    <Provider store={store}>
+        <App
+            strings={[
+                "Hi, I'm Henry.",
+                "I am a <strong><i>Software Developer.</i></strong>",
+                "I graduated from <strong>Purdue University.</strong>",
+                "I do <strong>Web Development.</strong>",
+                "I worked with various technologies!",
+                "I am trilingual.",
+                "I am a <strong>team player.</strong>",
+            ]}
+        />
+    </Provider>,
+    document.querySelector("#root"),
 );
