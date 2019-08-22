62677887dbd3dc082e58445b37f8cb48d8917d4e
lchoo <lchoo1996@gmail.com>
2019-08-21 22:47:02 -0700

    built: update react

--- personalwebsite/src/index.js
+++ personalwebsite/src/index.js
@@ -1,8 +1,8 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
 import store from "./store";
 import { Provider, ReactReduxContext } from "react-redux";
 
