50623e16545dd0d43836e3e0e0d29a40e11f466c
lchoo <lchoo1996@gmail.com>
2019-09-16 21:04:47 -0700

    update: animation to nav buttons

--- personalwebsite/src/components/Menu/Navbar.js
+++ personalwebsite/src/components/Menu/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { animateScroll as scroll } from "react-scroll";
-
+import { CSSTransition } from "react-transition-group";
 // activeClass - class applied when element is reached
 // to - target to scroll to
 // spy - make Link selected when scroll is at its targets position
@@ -9,34 +9,95 @@ import { animateScroll as scroll } from "react-scroll";
 // duration - time of the scroll animation, can be a number or a function
 
 class MenuBar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            load: false,
+            startTimer: false,
+            startTimer2: false,
+            btnOpacity: 0,
+            btnOpacity2: 0,
+        };
+    }
+    componentDidMount() {
+        this.setState({ load: true });
+        this.intervalId = setInterval(this.timer, 800);
+        this.intervalId2 = setInterval(this.timer2, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+        clearInterval(this.intervalId2);
+    }
+    timer = () => {
+        this.setState({ startTimer: true, btnOpacity: 1 });
+    };
+
+    timer2 = () => {
+        this.setState({ startTimer2: true, btnOpacity2: 1 });
+    };
+
     render() {
         return (
             <div className="ui menu__bar">
                 <div className="ui large secondary inverted pointing menu menu__flex">
                     <div className="item menu__flex--start" id="navProject">
-                        <a onClick={() => scroll.scrollTo(window.innerHeight)}>
-                            Projects
-                        </a>
+                        <CSSTransition
+                            in={this.state.load}
+                            timeout={2000}
+                            classNames="project-buttons"
+                            appear={true}
+                        >
+                            <a
+                                onClick={() =>
+                                    scroll.scrollTo(window.innerHeight)
+                                }
+                            >
+                                Projects
+                            </a>
+                        </CSSTransition>
                     </div>
 
                     <div className="right item menu__flex--end">
-                        <a
-                            className="ui inverted button"
-                            href="./resume.pdf"
-                            target="_blank"
-                            // download="resume.pdf"
+                        <CSSTransition
+                            in={this.state.startTimer}
+                            timeout={2000}
+                            classNames="resume-buttons"
+                            appear={this.state.startTimer}
                         >
-                            Resume
-                        </a>
+                            <div>
+                                <a
+                                    className="ui inverted button"
+                                    href="./resume.pdf"
+                                    target="_blank"
+                                    style={{ opacity: this.state.btnOpacity }}
+                                    // download="resume.pdf"
+                                >
+                                    Resume
+                                </a>
+                            </div>
+                        </CSSTransition>
                         &nbsp;
-                        <a
-                            className="ui inverted button"
-                            onClick={() =>
-                                scroll.scrollToBottom({ duration: 2000 })
-                            }
+                        <CSSTransition
+                            in={this.state.startTimer2}
+                            timeout={2000}
+                            classNames="contact-buttons"
+                            appear={this.state.startTimer2}
                         >
-                            Contact
-                        </a>
+                            <div>
+                                <a
+                                    className="ui inverted button"
+                                    onClick={() =>
+                                        scroll.scrollToBottom({
+                                            duration: 2000,
+                                        })
+                                    }
+                                    style={{ opacity: this.state.btnOpacity2 }}
+                                >
+                                    Contact
+                                </a>
+                            </div>
+                        </CSSTransition>
                     </div>
                 </div>
             </div>
